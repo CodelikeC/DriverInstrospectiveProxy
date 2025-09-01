959cd348096cf0f0f474c76c4548e11e598813af
ndtribk <ndtribk@gmail.com>
2025-09-01 12:45:27 +0700

    dip_init.c

--- Kernel/core/dip_init.c
+++ Kernel/core/dip_init.c
@@ -1,7 +1,26 @@
-#include <stdio.h>
+#include <ntddk.h> 
 
+// logging tiện dụng ,,.. 
+#define LOG(msg, ...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[DIP] " msg "\n", __VA_ARGS__ )
 
-int main ()
+void DriverUnload(PDRIVER_OBJECT DriverObject)
 {
-    return 0; 
+    UNREFERENCED_PARAMETER(DriverObject); 
+    LOG("Driver unloading....", 0); 
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+    UNREFERENCED_PARAMETER(RegistryPath);
+
+    LOG("DriverEntry called", 0);
+
+    DriverObject->DriverUnload = DriverUnload;
+
+    // Tại đây có thể: 
+    // - Khởi tạo hook 
+    // - Register với comm module
+    // - Gọi monitor logic từ kernel folder
+
+    LOG("DIP Kernel Init completed", 0);
+    return STATUS_SUCCESS;
 }
