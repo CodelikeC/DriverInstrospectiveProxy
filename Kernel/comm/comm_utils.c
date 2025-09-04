856ad9995dbf24975ee8b34a93db159763a26dd1
ndtribk <ndtribk@gmail.com>
2025-09-04 15:07:58 +0700

    comm_utils.c

--- Kernel/comm/comm_utils.c
+++ Kernel/comm/comm_utils.c
@@ -1,6 +1,15 @@
 #include <stdio.h> 
+#include "comm_channel.h"
 
-int main ()
+// ham log message .. // 
+void log_message(const CommMessage *msg)
 {
-    return 0; 
+    printf("LOG: From %s to %s | ID = %d | Payload = %s\n"
+    , msg -> sender, msg -> receiver , msg -> id, msg -> payload);
+}
+
+// ham dump toan bo buffer ,,
+void dump_channel_state()
+{
+    printf("Channel State : head - tail buffer dump(not yet implemented)\n"); 
 }
