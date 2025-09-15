e1ebab69126da3dadef9a137f42835289bd72eea
ndtribk <ndtribk@gmail.com>
2025-09-15 15:10:03 +0700

    hook_manager.c

--- Kernel/hook/hook_manager.c
+++ Kernel/hook/hook_manager.c
@@ -1,1 +1,53 @@
 #include "hook_manager.h"
+#include <string.h>
+
+#define MAX_HOOKS 128 
+static HookEntry hooks[MAX_HOOKS]; 
+static int hook_count = 0; 
+
+int hook_manager_init(){
+    hook_count = 0; 
+    return HOOK_OK; 
+}
+
+int hook_manager_add(const char *target, HookCallBack cb)
+{
+    if (hook_count >= MAX_HOOKS) return HOOK_ERROR; 
+
+    strncpy(hooks[hook_count].target, target, sizeof(hooks[hook_count].target) -1);
+    hooks[hook_count].id == hook_count; 
+    hooks[hook_count].callback == cb; 
+    hook_count++; 
+    return HOOK_OK; 
+}
+
+int hook_manager_remove(const char *target)
+{
+    for (int i = 0 ; i < hook_count ; i++)
+    {
+        if (strcmp(hooks[i].target, target) == 0)
+        {
+            hooks[i] = hooks[hook_count -1];
+            hook_count--; 
+            return HOOK_OK; 
+        }
+    }
+    return HOOK_NOT_FOUND; 
+}
+
+int hook_manager_invoke(const char *target, void *context)
+{
+    for (int i = 0 ; i < hook_count ; i++)
+    {
+        if (strcmp(hooks[i].target, target) == 0 && hooks[i].callback){
+            hooks[i].callback(target, context); 
+            return HOOK_OK; 
+        }
+    }
+    return HOOK_NOT_FOUND; 
+}
+
+void hook_manager_shutdown()
+{
+    hook_count = 0  ; 
+}
--- Kernel/hook/hook_manager.h
+++ Kernel/hook/hook_manager.h
@@ -1,1 +1,26 @@
+#pragma once 
 #include <stdio.h>
+#include <stddef.h>
+
+typedef enum 
+{
+    HOOK_OK = 0 , 
+    HOOK_ERROR = -1, 
+    HOOK_NOT_FOUND = -2
+}HookStatus; 
+
+typedef void (*HookCallBack)(const char *target, void *context); 
+
+typedef struct 
+{
+    int id; 
+    char target[64]; // ten ham syscall- driver 
+    HookCallBack callback; // ham xu ly khi bi hook
+}HookEntry;
+
+int hook_manager_init(); 
+int hook_manager_add(const char *target, HookCallBack cb);
+int hook_manager_remove(const char *target); 
+
+int hook_manager_invoke(const char *target, void *context); 
+void hook_manager_shutdown();
